8e29db424a6822e17917c0fe6d3fdd029eaffcf1
TrusTio <erkan.myumyunov@gmail.com>
2021-03-25 10:43:33 +0200

    Fixed Task repository

--- src/main/java/com/praktika/worktime/persistence/repository/RoleRepository.java
+++ src/main/java/com/praktika/worktime/persistence/repository/RoleRepository.java
@@ -2,12 +2,8 @@ package com.praktika.worktime.persistence.repository;
 
 
 import com.praktika.worktime.persistence.entity.Role;
-import com.praktika.worktime.persistence.entity.RoleName;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Integer> {
